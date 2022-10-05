9565b26202f702c52dc11d7c14cb624ee3425f18
adela50 <100993019+adela50@users.noreply.github.com>
2022-10-05 09:39:07 +0200

    Create zad3.sh

---
+++ bash/zad3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl http://api.nbp.pl/api/exchangerates/rates/a/usd/ ; echo ""
+curl http://api.nbp.pl/api/exchangerates/rates/a/gbp/ ; echo ""
+curl http://api.nbp.pl/api/exchangerates/rates/a/chf/ ; echo ""
+curl http://api.nbp.pl/api/exchangerates/rates/a/eur/ ; echo ""
