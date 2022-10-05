0ecd1759ad99f988c42735bd76baa0d5900efa9d
adela50 <100993019+adela50@users.noreply.github.com>
2022-10-05 09:34:38 +0200

    Create zad4.sh

---
+++ bash/zad4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Today is: "$(date +%c)
+echo "Wchich trains will arive soon?"
+echo "1. Practically MAYBE would arrive a train in 1 hour... but... we don't know wchich..."
+echo "2. As you see, we don't know, when would arrive the first train, so how we can know, when will arrive the second one and which train would it be?"
+echo "3. This train doesn't exist - it's ghost train..."
+echo "4. What's wrong with my train? I'm waiting for it for 2 hours... (or more...)"
+echo "5. Ohh... Finally I thought that my train won't arrive and I will come home on foot... (My brother once came home on foot... It took him ONLY 3 or 4 hours - it's faster than waiting for any trains :) )"
